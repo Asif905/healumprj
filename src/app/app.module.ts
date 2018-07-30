8f575109e7e8912189bff8f1c8b243bf8dcf9a99
Asif Zaidi <zaidia@mscdirect.com>
2018-07-31 00:56:21 +0530

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,33 +4,25 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { TextNoteComponent } from './text-note/text-note.component';
-import { ArticlesNoteComponent } from './articles-note/articles-note.component';
-import { PicturesNoteComponent } from './pictures-note/pictures-note.component';
-import { DocumentNoteComponent } from './document-note/document-note.component';
 import { AppRoutingModule } from './app.routing.module';
-import { TextNoteListComponent } from './text-note/text-note-list/text-note-list.component';
-import { ArticlesNoteListComponent } from './articles-note/articles-note-list/articles-note-list.component';
-import { DocumentNoteListComponent } from './document-note/document-note-list/document-note-list.component';
-import { PicturesNoteListComponent } from './pictures-note/pictures-note-list/pictures-note-list.component';
+import { ArticlesNoteModule } from './articles-note/articles-note.module';
+import { TextNoteModule } from './text-note/text-note.module';
+import { PictureNoteModule } from './pictures-note/pictures-note.module';
+import { DocumentNoteModule } from './document-note/document-note.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    TextNoteComponent,
-    ArticlesNoteComponent,
-    PicturesNoteComponent,
-    DocumentNoteComponent,
-    TextNoteListComponent,
-    ArticlesNoteListComponent,
-    DocumentNoteListComponent,
-    PicturesNoteListComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ArticlesNoteModule,
+    TextNoteModule,
+    PictureNoteModule,
+    DocumentNoteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,16 +1,8 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router';
-import { TextNoteComponent } from "./text-note/text-note.component";
-import { ArticlesNoteComponent } from "./articles-note/articles-note.component";
-import { PicturesNoteComponent } from "./pictures-note/pictures-note.component";
-import { DocumentNoteComponent } from "./document-note/document-note.component";
 
 const appRoutes: Routes = [
-    {path: '', redirectTo: 'textNote', pathMatch: 'full'},
-    { path: 'textNote', component: TextNoteComponent},
-    { path: 'articleNote', component: ArticlesNoteComponent},
-    { path: 'pictureNote', component: PicturesNoteComponent},
-    { path: 'documentNote', component: DocumentNoteComponent}
+    { path: '', redirectTo: 'textNote', pathMatch: 'full'}
 ]
 
 @NgModule({
